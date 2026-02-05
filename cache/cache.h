f6eb2ac6d92c547afbe0de8c1212cbf7fa5a6696
bug00n <daniltulgaev3837@gmail.com>
2026-02-06 01:59:05 +0300

    feat: async multi-threaded Cache has been written

--- cache/cache.h
+++ cache/cache.h
@@ -4,12 +4,18 @@
 #include <unordered_map>
 #include <vector>
 #include <optional>
+#include <shared_mutex>
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <atomic>
 
 
 template <typename KeyType, typename ValueType>
 class Shard {
  private:
     std::unordered_map <KeyType, ValueType> shard_storage;
+    mutable std::shared_mutex mutex;
 
  public:
     Shard() = default;
@@ -21,20 +27,38 @@ class Shard {
     void remove(const KeyType& key);
 };
 
+template <typename Task>
+class TaskQueue {
+ private:
+    std::mutex mutex;
+    std::condition_variable cv;
+    std::queue <Task> queue;
+
+ public:
+    void push(const Task& task);
+
+    Task pop(std::atomic<bool>& stop_flag);
+};
 
 template <typename KeyType, typename ValueType>
 class Cache {
  private:
     std::vector <Shard<KeyType, ValueType>> cache_storage;
+    std::vector <std::thread> threads;
+    TaskQueue <std::function <void()>> tasks;
+    std::atomic <bool> stop{false};
 
     size_t getId(const KeyType& key) const;
 
+    void thread_loop();
+
  public:
-    Cache(int size) explicit;
+    explicit Cache(int count_of_shards, int count_of_threads);
+    ~Cache() noexcept;
 
-    std::optional<ValueType> get(const KeyType& key) const;
+    std::future<std::optional<ValueType>> get_async(const KeyType& key);
 
-    void set(const KeyType& key, const ValueType& value);
+    void set_async(const KeyType& key, const ValueType& value);
 
-    void remove(const KeyType& key);
+    void remove_async(const KeyType& key);
 };
